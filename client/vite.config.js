81e1148d55f56759dfe6cbf91245fe19f0d9d0a0
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-08-05 10:38:13 +0530

    added backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://smart-city-mern-backend.vercel.app",
         secure: false,
       },
     },
