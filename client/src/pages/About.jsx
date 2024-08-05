9c7734d376e54e88126376433e27f30279a8b075
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2024-08-05 10:16:21 +0530

    initial commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Smart City</h1>
+      <p className='mb-4 text-slate-700'>Welcome to the future of urban living with the Smart City app. Embrace a new era of convenience and innovation as we redefine your real estate journey. Our app is your gateway to a curated selection of properties in the most advanced and interconnected cities.</p>
+      <p className='mb-4 text-slate-700'>
+        Discover a seamless experience with our cutting-edge features. Immerse yourself in property exploration like never before with augmented reality tours that bring listings to life. Your home search is now personalized and smarter, thanks to integrated technologies that enhance and simplify your daily life.
+      </p>
+      <p className='mb-4 text-slate-700'>We believe in informed decision-making, and our app empowers you with in-depth analytics to guide your choices. Dive into a world where data meets design, ensuring that your next property is not just a space but a carefully curated lifestyle.
+      </p>
+      <p className='mb-4 text-slate-700'>Welcome to the Smart City app — where the pulse of modern urban living meets the heartbeat of tomorrow. Experience the future of real estate, today.
+      </p>
+    </div>
+  )
+}
